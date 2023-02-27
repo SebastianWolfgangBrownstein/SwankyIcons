015b4330e3661816d5d152d3a81f31c4d67ff615
SebastianBrownstein <sbrownstein.dev@gmail.com>
2023-02-27 00:22:54 -0700

    fixed rollup config input path

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -10,7 +10,7 @@ import packageJson from "./package.json" assert { type: "json" };
 
 export default [
     {
-        input: "/src/index.ts",
+        input: "./src/index.ts",
         output: [
             {
                 file: packageJson.main,
